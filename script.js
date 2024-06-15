95b46ef9d73293a7f7305d26bc00a463c3e1795f
jellyfish <rvupmo33@gmail.com>
2024-06-15 11:36:21 -0400

    updated js-learning

--- script.js
+++ script.js
@@ -10,3 +10,53 @@ btn.addEventListener("click", function () {
     btn.textContent = "share a fact";
   }
 });
+
+// function calcFactAge(FactYear) {
+//   const currentYear = new Date().getFullYear();
+//   const age = currentYear - FactYear;
+//   return age;
+// }
+// console.log(`The fact is ${calcFactAge(2015)} years old`);
+// console.log(`The fact is ${calcFactAge(2020)} years old`);
+
+// let votesInteresting = 20;
+// let votesMindblowing = 5;
+
+// let votesFalse = 7;
+// const totalUpVotes = votesInteresting + votesMindblowing;
+
+// const message =
+//   totalUpVotes > votesFalse
+//     ? "The fact might be true"
+//     : "The fact might be false";
+
+// // console.log(message);
+
+// const text = "Cow goes Moo";
+// const upperText = text.toUpperCase();
+// console.log(upperText);
+// const str = `The current fact is "${text}"`;
+// console.log(str);
+
+// const fact = ["Cow goes Moo", 2015, true];
+// console.log(fact);
+// console.log(fact[2]);
+// console.log(fact.length);
+// console.log(fact[0].length);
+// const [text, createdIn, isCorrect] = fact;
+// console.log(text);
+
+// const newFact = [...fact, "society"];
+// console.log(newFact);
+
+// const factObj = {
+//   text: "Cow goes Moo",
+//   category: "society",
+//   createdIn: 2015,
+//   isTrue: true,
+//   createSummary: function () {
+//     return `The fact "${this.text}"`;
+//   },
+// };
+
+// console.log(factObj.createSummary());
