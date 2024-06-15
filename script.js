ca9fe3c6b919b6f69e544dcdb82aba7156137e93
jellyfish <rvupmo33@gmail.com>
2024-06-15 10:46:27 -0400

    day 1 progress

---
+++ script.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector(".btnOpen");
+const form = document.querySelector(".fact-form");
+
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "share a fact";
+  }
+});
